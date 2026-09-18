b5c51d76c98a84a1f1b104b62e6868b90afc180e
Zi Heng Ewe <92201025+GodEwewer@users.noreply.github.com>
2026-09-18 12:58:34 +0800

    Fix window selection and custom recording hotkey

--- src/preload.js
+++ src/preload.js
@@ -2,6 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('clickfilm', {
   setRecordingHotkey: accelerator => ipcRenderer.invoke('hotkey:set', accelerator),
+  listCaptureSources: () => ipcRenderer.invoke('capture:list-sources'),
+  selectCaptureSource: sourceId => ipcRenderer.invoke('capture:select-source', sourceId),
   onRecordingToggle: callback => {
     const listener = () => callback();
     ipcRenderer.on('recording:toggle', listener);
