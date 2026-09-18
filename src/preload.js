2bbaf845e5cf90fb3992d5681123f05876d942b1
Zi Heng Ewe <92201025+GodEwewer@users.noreply.github.com>
2026-09-18 14:53:11 +0800

    Add native-resolution export and multi-app audio mixer

--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,9 @@ contextBridge.exposeInMainWorld('clickfilm', {
   setRecordingHotkey: accelerator => ipcRenderer.invoke('hotkey:set', accelerator),
   listCaptureSources: () => ipcRenderer.invoke('capture:list-sources'),
   selectCaptureSource: sourceId => ipcRenderer.invoke('capture:select-source', sourceId),
+  listAudioApps: () => ipcRenderer.invoke('audio:list-apps'),
+  startAudioCapture: pids => ipcRenderer.invoke('audio:start', pids),
+  stopAudioCapture: () => ipcRenderer.invoke('audio:stop'),
   onRecordingToggle: callback => {
     const listener = () => callback();
     ipcRenderer.on('recording:toggle', listener);
